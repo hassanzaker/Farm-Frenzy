5e1ee1f1037d2bd2e9a4545007dc108e032245f8
aliabbasishask <aliabbasi7898@gmail.com>
2018-12-13 23:42:49 +0330

    Crash completed and some thing in ground changed

--- src/Model/Animals/WildAnimal.java
+++ src/Model/Animals/WildAnimal.java
@@ -2,12 +2,13 @@ package Model.Animals;
 
 import Model.Animals.Animal;
 import Model.Cell;
+import Model.Ground;
 
 public abstract class WildAnimal extends Animal {
     protected int volume;
 
-    public WildAnimal(int x, int y) {
-        super(x, y);
+    public WildAnimal(int x, int y , String ID) {
+        super(x, y , ID);
     }
 
     public int getVolume() {
@@ -23,4 +24,31 @@ public abstract class WildAnimal extends Animal {
     }
 
     public abstract void eat();
+    public void crash(Ground ground)
+    {
+      for(int i=0 ; i < ground.getProducerAnimals().size() ; i++) {
+          if(ground.getProducerAnimals().get(i).getRow() == this.row &&
+                  ground.getProducerAnimals().get(i).getColumn()== this.column) {
+              ground.deleteAnimal(ground.getProducerAnimals().get(i));
+          }
+      }
+      for(int i=0 ; i < ground.getCats().size() ; i++) {
+          if(ground.getCats().get(i).getRow() == this.row &&
+                  ground.getCats().get(i).getColumn() == this.column) {
+                ground.deleteAnimal(ground.getCats().get(i));
+            }
+      }
+      for(int i=0 ; i < ground.getDogs().size() ; i++) {
+          if(ground.getDogs().get(i).getRow() == this.row &&
+                  ground.getDogs().get(i).getColumn() == this.column) {
+              ground.deleteAnimal(ground.getDogs().get(i));
+          }
+      }
+      for(int i=0 ; i < ground.getItems().size() ; i++) {
+          if(ground.getItems().get(i).getRow() == this.row &&
+                  ground.getItems().get(i).getColumn() == this.column) {
+              ground.deleteItem(ground.getItems().get(i));
+          }
+      }
+    }
 }
